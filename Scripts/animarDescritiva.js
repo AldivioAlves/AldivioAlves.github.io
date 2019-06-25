3b624f6af3c434d2fb6fea9d7c9362e3f79186f4
Aldivio <aldivio89@hotmail.com>
2019-06-24 22:45:58 -0300

    bug animation fixed

--- Scripts/animarDescritiva.js
+++ Scripts/animarDescritiva.js
@@ -3,7 +3,7 @@ animarDescritiva = function (solicitante) {
 
 
     CaixaD = document.getElementById('descritiva')
-    CaixaD.style.overflow='hidden'
+    CaixaD.style.overflow = 'hidden'
     tInicial = document.getElementById('descritiva').offsetHeight - 7
 
     if (solicitante == 'arquivo') {
@@ -11,15 +11,15 @@ animarDescritiva = function (solicitante) {
             return
         }
         if (tOrdinal) {
-            
+
             document.getElementById('caixa').innerHTML = ''
             document.getElementById('formulario').variavel.value = "Nominal"
-          
-            tManual=false
+
+            tManual = false
         }
         if (tManual) {
 
-            tFinal = tInicial-10
+            tFinal = tInicial - 10
         }
         else {
             tFinal = 410
@@ -33,12 +33,12 @@ animarDescritiva = function (solicitante) {
     if (solicitante == 'manual') {
 
         if (tOrdinal) {
-            
+
             document.getElementById('caixa').innerHTML = ''
             document.getElementById('formulario').variavel.value = "Nominal"
-         
-            tArquivo=false
-            tOrdinal=false
+
+            tArquivo = false
+            tOrdinal = false
         }
         if (tManual) {
             return
@@ -51,25 +51,27 @@ animarDescritiva = function (solicitante) {
         }
         tManual = true
         tArquivo = false
-        tOrdinal=false
+        tOrdinal = false
     }
 
     if (solicitante == 'AP') {
         if (tDisCont) {
             return
         }
- 
-        
-        if(tOrdinal && tManual){
-            tFinal=500
+
+
+        if (tOrdinal && tManual) {
+            tFinal = 500
         }
-        if(!tManual&&!tArquivo){
-            tFinal= 413
+        if (!tManual && !tArquivo) {
+            alert('aqui')
+            tFinal = 400
         }
-        if(tManual){
-            tFinal=500
+        if (tManual) {
+            tFinal = 500
         }
-        else {
+        if (tArquivo) {
+
             tFinal = 480
         }
         tDisCont = true
@@ -80,26 +82,32 @@ animarDescritiva = function (solicitante) {
     if (solicitante == 'nominal') {
         if (tNominal || !tDisCont) {
             return
-        } 
-        if(!tArquivo&&!tManual){
-            return
+        }
+
+        if (!tArquivo && !tManual) {
+            if (tDisCont) {
+                tFinal = 335
+            }
+            else {
+                return
+            }
         }
         if (tDisCont && tManual) {
 
-            tFinal = 420    
+            tFinal = 420
         }
-        if(tDisCont && tArquivo){
-            tFinal=410
+        if (tDisCont && tArquivo) {
+            tFinal = 410
         }
 
         if (tOrdinal && tManual) {
-            
+
             tFinal = 427
         }
-        if(tOrdinal && tArquivo){
-            tFinal=410
+        if (tOrdinal && tArquivo) {
+            tFinal = 410
         }
-        
+
         tOrdinal = false
         tDisCont = false
         tNominal = true
@@ -112,18 +120,18 @@ animarDescritiva = function (solicitante) {
             return
         }
         Hselets = document.getElementById('caixaSelets').offsetHeight + 21
-        tFinal = String(tInicial + Hselets +50)
+        tFinal = String(tInicial + Hselets + 50)
 
-        if(tDisCont){
-            
-            tFinal= String(tInicial + Hselets-13)
+        if (tDisCont) {
+
+            tFinal = String(tInicial + Hselets - 13)
         }
         tOrdinal = true
         tNominal = false
         tDisCont = false
     }
-//alert(` solicitante = ${solicitante}\n Incial= ${tInicial} final= ${tFinal}`)
-   
-CaixaD.animate([{ height: String(tInicial) + 'px' }, { height: String(tFinal) + 'px' }], { duration: 200 })
-CaixaD.style.height=String(tFinal)+'px'
+    //alert(` solicitante = ${solicitante}\n Incial= ${tInicial} final= ${tFinal}`)
+
+    CaixaD.animate([{ height: String(tInicial) + 'px' }, { height: String(tFinal) + 'px' }], { duration: 200 })
+    CaixaD.style.height = String(tFinal) + 'px'
 }
